3fb5c7a6318c362ddc0644e6921b80338952c735
Eugene Shved <113920204+Eugene-doctor@users.noreply.github.com>
2023-06-16 11:37:22 +0500

    Update greeting.go

--- greeting.go
+++ greeting.go
@@ -1,11 +1,14 @@
+//This print "Hello!" or "Hi!"
 package greeting 
 
 import "fmt" 
 
+//This print "Hello!"
 func Hello() { 
     fmt.Println("Hello!")
 } 
 
+//This print "Hi!"
 func Hi() {
     fmt.Println("Hi!") 
 }
